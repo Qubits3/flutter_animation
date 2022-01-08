91dcbcadb36954b4f40d62489ff15b98ec52a8a2
Metin Yıldıran <metin.1464@gmail.com>
2022-01-08 17:08:27 +0300

    Finished Hero

--- lib/shared/screenTitle.dart
+++ lib/shared/screenTitle.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class ScreenTitle extends StatelessWidget {
@@ -7,9 +9,17 @@ class ScreenTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+    return TweenAnimationBuilder(
+      duration: const Duration(milliseconds: 500),
+      tween: Tween<double>(begin: 0, end: 1),
+      builder: (BuildContext context, double _val, Widget? child) {
+        return Opacity(opacity: _val,
+        child: Padding(padding: EdgeInsets.only(top: _val * 20), child: child,));
+      },
+      child: Text(
+        text,
+        style: const TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
